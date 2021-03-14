9bf011bcb7d2d1bc475315acfb64e905bd492a88
akaohiroki <akayoshi222@gmail.com>
2021-03-14 20:44:09 +0900

    投稿一覧表示機能の実装

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,7 +2,7 @@ class ArticlesController < ApplicationController
   before_action :authenticate_user!, except: :index
 
   def index
-    @articles = Article.order("created_at DESC")
+    @articles = Article.order("created_at DESC").includes(:user)
   end
 
   def new
