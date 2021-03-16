aa7836dbd647a128711702ff6d2acd04f698b24a
akaohiroki <akayoshi222@gmail.com>
2021-03-16 18:47:43 +0900

    ルーティング、コントローラーの作成

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
     registrations: 'users/registrations'
   }
   root to: "articles#index"
-  resources :articles
+  resources :articles do
+    resources :comments, only: :create
+  end
   resources :users, only: :show
 end
