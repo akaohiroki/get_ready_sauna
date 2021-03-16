82649474d3ab31d3f131f58193bcab2b60726c72
akaohiroki <akayoshi222@gmail.com>
2021-03-16 17:12:53 +0900

    ユーザー編集ページの実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,24 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
   end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      bypass_sign_in(@user)
+      redirect_to user_path(@user.id)
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:image, :nickname, :password, :age, :gender_id, :sauna_history_id, :self_introduction)
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -15,6 +15,19 @@ class User < ApplicationRecord
   validates :self_introduction, length: { maximum: 1000 }
   validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i }
 
+  def update_without_current_password(params, *options)
+    params.delete(:current_password)
+
+    if params[:password].blank? && params[:password_confirmation].blank?
+      params.delete(:password)
+      params.delete(:password_confirmation)
+    end
+
+    result = update_attributes(params, *options)
+    clean_up_passwords
+    result
+  end
+
   has_many :articles
   has_one_attached :image
   extend ActiveHash::Associations::ActiveRecordExtensions
