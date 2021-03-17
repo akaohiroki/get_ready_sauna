23481f4e3e812129eaf0a84ca480cec4b6101fab
akaohiroki <akayoshi222@gmail.com>
2021-03-17 22:16:40 +0900

    ActiveHashモデルの修正

--- app/models/break_space.rb
+++ app/models/break_space.rb
@@ -1,12 +1,12 @@
 class BreakSpace < ActiveHash::Base
   self.data = [
-    { id: 1, name: '1.--' },
-    { id: 2, name: '2.狭め 外気浴なし' },
-    { id: 3, name: '3.普通 外気浴なし' },
-    { id: 4, name: '4.広い 外気浴なし' },
-    { id: 5, name: '5.狭め 外気浴あり' },
-    { id: 6, name: '6.普通 外気浴あり' },
-    { id: 7, name: '7.広い 外気浴あり' }
+    { id: 1, name: '--' },
+    { id: 2, name: '狭め 外気浴なし' },
+    { id: 3, name: '普通 外気浴なし' },
+    { id: 4, name: '広い 外気浴なし' },
+    { id: 5, name: '狭め 外気浴あり' },
+    { id: 6, name: '普通 外気浴あり' },
+    { id: 7, name: '広い 外気浴あり' }
   ]
 
   include ActiveHash::Associations
