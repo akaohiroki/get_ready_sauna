9959639ebf162da8d3b18f4cfb64a4d099eaeaec
akaohiroki <akayoshi222@gmail.com>
2021-03-19 16:27:43 +0900

    新規登録画面プレビュー機能の実装

--- app/javascript/preview.js
+++ app/javascript/preview.js
@@ -1,8 +1,7 @@
-if (document.URL.match( /new/ ) || document.URL.match( /edit/ )) {
+if (document.URL.match( /new/ ) || document.URL.match( /edit/ ) || document.URL.match( /sign_up/ )) {
   document.addEventListener('DOMContentLoaded', function(){
     const ImageList = document.getElementById('image-list');
 
-    // 選択した画像を表示する関数
     const createImageHTML = (blob) => {
        // 画像を表示するためのdiv要素を生成
       const imageElement = document.createElement('div');
@@ -10,6 +9,7 @@ if (document.URL.match( /new/ ) || document.URL.match( /edit/ )) {
       // 表示する画像を生成
       const blobImage = document.createElement('img');
       blobImage.setAttribute('src', blob);
+
       // 生成したHTMLの要素をブラウザに表示させる
       imageElement.appendChild(blobImage);
       ImageList.appendChild(imageElement);
