95c68aac49196fc3ac55a317a9a300ddc99c97a3
akaohiroki <akayoshi222@gmail.com>
2021-03-23 19:30:10 +0900

    コントローラーの編集

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -2,19 +2,16 @@ class LikesController < ApplicationController
   before_action :set_variables, only: [:create, :destroy]
 
   def create
-    like = current_user.likes.new(article_id: @article.id)
-    like.save
+    Like.create(user_id: current_user.id, article_id: params[:id])
   end
 
   def destroy
-    like = current_user.likes.find_by(article_id: @article.id)
-    like.destroy
+    Like.find_by(user_id: current_user.id, article_id: params[:id]).destroy
   end
 
   private 
 
   def set_variables
-    @article = Article.find(params[:article_id])
-    @id_name = "#like-link-#{@article.id}"
+    @article = Article.find(params[:id])
   end
 end
