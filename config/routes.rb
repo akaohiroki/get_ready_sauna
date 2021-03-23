b11c3e060efc8fc6a5b1db37031dbd1d669d354b
akaohiroki <akayoshi222@gmail.com>
2021-03-23 19:22:37 +0900

    ルーティングの編集

--- config/routes.rb
+++ config/routes.rb
@@ -5,10 +5,11 @@ Rails.application.routes.draw do
   root to: "articles#index"
   resources :articles do
     resources :comments, only: [:create, :destroy]
-    resources :likes, only: [:create, :destroy]
     collection do
       get 'search'
     end
   end
   resources :users, only: [:show, :edit, :update]
+  post   '/like/:id' => 'likes#create',   as: 'create_like'
+  delete '/like/:id' => 'likes#destroy',  as: 'destroy_like'
 end
