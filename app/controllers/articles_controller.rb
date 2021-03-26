d322e3c83e4f6e8ebaf7ae9f6b1107f8819b0eed
akaohiroki <akayoshi222@gmail.com>
2021-03-26 12:32:01 +0900

    articles_controllerの修正

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -14,7 +14,7 @@ class ArticlesController < ApplicationController
   def create
     @article = Article.new(article_params)
     if @article.save
-      redirect_to root_path
+      redirect_to root_path(anchor: 'articles-lists')
     else
       render :new
     end
